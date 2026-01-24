329ca84abdcdd40d454946ad31f20f16ff604eb5
amaebo <amaebong20033@gmail.com>
2026-01-24 00:06:18 -0700

    Refactor database initialization to improve readability, structure and security

--- db/db_init.py
+++ db/db_init.py
@@ -1,9 +1,35 @@
 import sqlite3
+import os
 
-#connect to database
-conn = sqlite3.connect('ground_system.db')
-cur = conn.cursor()
+# simple paths so its easy to read
+BASE_DIR = os.path.dirname(__file__)
+DB_PATH = os.path.join(BASE_DIR, "ground_system.db")
+SCHEMA_PATH = os.path.join(BASE_DIR, "schema.sql")
 
 
-# create table
-cur.execute("""  )""")
+def get_db(db_path=None):
+    """
+    Returns a conn object to database
+    
+    :param db_path: Path to database file
+    """
+    path = db_path or DB_PATH
+    conn = sqlite3.connect(path)
+    conn.execute("PRAGMA foreign_keys = ON;")
+    return conn
+
+
+def init_db(db_path=None, schema_path=None):
+    """
+    Initializes Database
+
+    :param db_path: Path to database file
+    :param schema_path: Path to database schema creation file
+    """
+    path = schema_path or SCHEMA_PATH
+    with open(path, "r") as f:
+        schema_sql = f.read()
+    conn = get_db(db_path)
+    conn.executescript(schema_sql)
+    conn.commit()
+    conn.close()
