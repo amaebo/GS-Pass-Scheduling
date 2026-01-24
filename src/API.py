78f3a6bf04f9da9932ad900f7f564cb96f5181a3
amaebo <amaebong20033@gmail.com>
2026-01-23 23:31:13 -0700

    Add satellite registration endpoint with validation

--- src/API.py
+++ src/API.py
@@ -4,3 +4,22 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class Satellite(BaseModel):
+    norad_id: int
+    s_name: str
+    
+# Satellite Registraion 
+@app.post("/satellites/register")
+def register_satellite(satellite: Satellite ):
+    satellite_dict = satellite.model_dump()
+    message = ""
+
+    if satellite_dict["norad_id"] and satellite_dict["s_name"]:
+        message = "Satellite registered"
+    else:
+        message = "Error: missing variables" 
+
+    return {
+        "message" : message,
+        "satellite" : satellite_dict
+    }
