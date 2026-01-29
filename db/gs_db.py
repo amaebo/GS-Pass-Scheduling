2ec097c2552160ee7cbd6d15c183da1ff1b1556d
amaebo <amaebong20033@gmail.com>
2026-01-29 01:54:37 -0700

    Refactor insert_gs_manual function to correct logic

--- db/gs_db.py
+++ db/gs_db.py
@@ -3,15 +3,16 @@ import sqlite3
 from db.db_init import db_connect
 
 
-def insert_gs_manual(gs_name: str, lon: float, lat: float):
+def insert_gs_manual(gs_name: str, lon: float, lat: float) -> int:
     query= """
-            INSERT INTO ground_stations(gs_name, lon, lat, source) VALUES (?,?,?, manual)
+            INSERT INTO ground_stations(gs_name, lon, lat, source, status) 
+            VALUES (?,?,?,?,?);
         """
     conn = db_connect()
     try:
-        cur = conn.execute(query, gs_name, lon, lat)
+        cur = conn.execute(query, (gs_name, lon, lat, "manual", "ACTIVE"))
         conn.commit()
-        return cur.lastrowid # return primary key of new row (s_id)
+        return cur.lastrowid # return primary key of new row (gs_id)
     except sqlite3.Error:
         conn.rollback()
         raise
