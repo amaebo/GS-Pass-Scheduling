a9c1310a91619978282fc3b1403f7f14d9358c6d
amaebo <amaebong20033@gmail.com>
2026-01-30 21:20:34 -0700

    get_mission_by_id and update_misson function

--- db/missions_db.py
+++ db/missions_db.py
@@ -20,7 +20,7 @@ def add_mission(mission_name: str, owner: str | None = None, priority: str | Non
     finally:
         conn.close()
 
-def get_missions():
+def get_all_missions():
     query = """
             SELECT *
             FROM missions;
@@ -36,3 +36,52 @@ def get_missions():
         raise
     finally:
         conn.close()
+
+def get_mission_by_id(mission_id: int):
+    query= """
+            SELECT *
+            FROM missions
+            WHERE mission_id = ?
+        """
+    conn = db_connect()
+    
+    try:
+        cur = conn.execute(query, (mission_id,))
+        conn.commit()
+        return cur.fetchone()
+    except sqlite3.Error:
+        conn.rollback()
+        raise
+    finally:
+        conn.close()
+
+def update_mission(mission_id: int, updates: dict) -> int:
+    if not updates:
+        return 0
+
+    set_clauses = []
+    params = []
+
+    for field, value in updates.items():
+        set_clauses.append(f"{field} = ?")
+        params.append(value)
+    
+    params.append(mission_id)
+
+
+    query = f"""
+            UPDATE missions
+            SET {", ".join(set_clauses)}
+            WHERE mission_id = ?;
+        """
+    conn = db_connect()
+    try:
+        cur = conn.execute(query, params)
+        conn.commit()
+        return cur.rowcount
+    except sqlite3.Error:
+        conn.rollback()
+        raise
+    finally:
+        conn.close()
+    
