79009562e7017c828cb1607bfc76356ada77e01b
amaebo <amaebong20033@gmail.com>
2026-02-01 02:40:59 -0700

    function to get passes from N2YO API

--- src/services/n2yo_client.py
+++ src/services/n2yo_client.py
@@ -1,6 +1,7 @@
-import requests
+import httpx
+from fastapi import HTTPException
 
-from src.core.config import N2YO_API_BASE_URL, N2YO_API_KEY
+from src.core.config import N2YO_BASE_URL, N2YO_API_KEY
 
 
 def get_passes_from_n2yo(
@@ -10,10 +11,17 @@ def get_passes_from_n2yo(
     alt: float,
     min_visibility: int,
     days: int = 1,
-):
-    """Send api requests to get passes form n2yo API."""
-    res = requests.get(
-        f"{N2YO_API_BASE_URL}visualpasses/{norad_id}/{gs_lat}/{gs_lon}/{alt}/{days}/{min_visibility}",
-        params={"apiKey": N2YO_API_KEY} if N2YO_API_KEY else None,
+) -> dict[str, object]:
+    """Send N2YO API request to get passes.
+
+    Returns parsed JSON response from NY2O API.
+    """
+    if not N2YO_API_KEY:
+        raise HTTPException(status_code=500, detail="N2YO API key is not configured.")
+    if not N2YO_BASE_URL:
+        raise HTTPException(status_code=500, detail="N2YO API base url is not configured.")
+    res = httpx.get(
+        f"{N2YO_BASE_URL}visualpasses/{norad_id}/{gs_lat}/{gs_lon}/{alt}/{days}/{min_visibility}",
+        params={"apiKey": N2YO_API_KEY}
     )
-    return res
+    return res.json()
