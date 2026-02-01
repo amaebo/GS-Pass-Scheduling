6473da668eb2e503e2e7973a961651a72d700c53
amaebo <amaebong20033@gmail.com>
2026-02-01 00:21:50 -0700

    Added config.py centralizing environment configurations and refactoring passes.py to use config

--- src/routers/passes.py
+++ src/routers/passes.py
@@ -1,13 +1,37 @@
-import os
-from pathlib import Path
-
 import requests
-from dotenv import load_dotenv
 from fastapi import APIRouter
+
+from src.core.config import N2YO_API_BASE_URL, N2YO_API_KEY
 
 
 router = APIRouter()
 
-load_dotenv(Path(__file__).resolve().parents[2] / ".env")
-N2YO_API_KEY = os.getenv("N2YO_API_KEY")
-N2YO_BASE_URL = os.getenv("N2YO_API_BASE_URL")
+
+@router.get("/passes")
+def view_pass(s_id: int, gs_id: int):
+    pass
+    # Check API for new passes
+
+    # Check cache for passes
+
+    # Check api
+
+
+def get_passes_from_n2yo(
+    norad_id: int,
+    gs_lon: float,
+    gs_lat: float,
+    alt: float,
+    min_visibility: int,
+    days: int = 1,
+):
+    """Send api requests to get passes form n2yo API."""
+    res = requests.get(
+        f"{N2YO_API_BASE_URL}visualpasses/{norad_id}/{gs_lat}/{gs_lon}/{alt}/{days}/{min_visibility}",
+        params={"apiKey": N2YO_API_KEY} if N2YO_API_KEY else None,
+    )
+    return res
+
+
+def cache_passes():
+    """Get satellite-groundstation passes from API."""
