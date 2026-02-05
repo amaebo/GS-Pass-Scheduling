2a7e12da126e12ef9021cc4882b1834467ed8f71
amaebo <amaebong20033@gmail.com>
2026-02-05 02:21:27 -0700

    add reservation queries to get reservation by r_id and get reservation commands by r_id

--- db/reservations_db.py
+++ db/reservations_db.py
@@ -1,4 +1,4 @@
-from db.db_query import execute_row_id
+from db.db_query import execute_row_id, fetch_one, fetch_all
 
 def insert_reservation(pass_id: int, gs_id: int, s_id: int, mission_id: int | None ):
     query = """
@@ -14,3 +14,19 @@ def add_command_to_reservation(r_id: int, command: list[str] | None):
             VALUES (?, ?)
         """
     return execute_row_id(query,(r_id, command))
+
+def get_reservation_by_r_id(r_id: int):
+    query = """
+            SELECT *
+            FROM reservations 
+            WHERE r_id = ?
+        """
+    return fetch_one(query,(r_id,))
+
+def get_reservation_commands_by_r_id(r_id:int):
+    query = """
+            SELECT command_type
+            FROM reservation_commands
+            WHERE r_id = ?
+        """
+    return fetch_all(query,(r_id,))
