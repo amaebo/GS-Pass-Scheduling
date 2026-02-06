4a90cbd3b60a5501caaa71bfbcfd453a9a2888a9
amaebo <amaebong20033@gmail.com>
2026-02-06 06:25:10 -0700

    gs query functions to check for active reservations with gs and delete gs

--- db/gs_db.py
+++ db/gs_db.py
@@ -1,6 +1,6 @@
 
 import sqlite3
-from db.db_query import execute_row_id, fetch_all, fetch_one
+from db.db_query import execute_row_id, execute_rowcount, fetch_all, fetch_one
 
 
 def insert_gs_manual(gs_code: str, lon: float, lat: float, alt: float) -> int:
@@ -30,5 +30,28 @@ def get_gs_by_id(gs_id: int):
         """
     try:
         return fetch_one(query, (gs_id,))
+    except sqlite3.Error:
+        raise
+    
+def gs_has_active_reservations(gs_id: int) -> bool:
+    query = """
+            SELECT 1
+            FROM reservations r
+            INNER JOIN predicted_passes p ON p.pass_id = r.pass_id
+            WHERE r.gs_id = ?
+              AND r.cancelled_at IS NULL
+              AND p.end_time >= CURRENT_TIMESTAMP
+            LIMIT 1
+        """
+    return True if fetch_one(query, (gs_id,)) else False
+    
+
+def delete_gs_by_id(gs_id: int) -> int:
+    query = """
+            DELETE FROM ground_stations
+            WHERE gs_id = ?
+        """
+    try:
+        return execute_rowcount(query, (gs_id,))
     except sqlite3.Error:
         raise
